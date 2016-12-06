690f917231915f81970d5c5280906e6ea1e70c5a
George Eaton <george_eaton@hotmail.co.uk>
2016-12-06 13:57:35 +0000

    for asuka

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -5,6 +5,7 @@ describe("fizzBuzz", function(){
     fizzbuzz = new fizzBuzz();
   });
 
+  describe("is_divisible_by", function() {
 
     it("should be able to return 1", function(){
       var number = 1;
@@ -15,7 +16,6 @@ describe("fizzBuzz", function(){
       var number = 3;
       expect(fizzbuzz.isDivisibleBy(3, number)).toEqual(true);
     });
-
     it("should know if the number is not divisible by 3", function(){
       var number = 4;
       expect(fizzbuzz.isDivisibleBy(3, number)).toEqual(false);
@@ -25,7 +25,6 @@ describe("fizzBuzz", function(){
       var number = 5;
       expect(fizzbuzz.isDivisibleBy(5, number)).toEqual(true);
     });
-
     it("should know if the number is not divisible by 5", function(){
       var number = 7;
       expect(fizzbuzz.isDivisibleBy(5, number)).toEqual(false);
@@ -39,7 +38,12 @@ describe("fizzBuzz", function(){
     it("should know if the number is not divisible by 3 and 5", function() {
       var number = 14;
       expect(fizzbuzz.isDivisibleBy(15, number)).toEqual(false);
-
     });
-
+  });
+  describe("fizz", function() {
+    it("should return fizz when passed a multiple of 3", function() {
+      var number = 3;
+      expect(fizzbuzz.says(number).toEqual('fizz');
+    });
+    });
 });
